f292c8e1320421078b1915dbf7475193256ad59c
Tim <tim.b.schneider@gmail.com>
2022-07-10 03:01:21 -0700

    send it, don't bend it

---
+++ src/SortingAlgorithms/MergeSort.js
@@ -0,0 +1,188 @@
+
+
+export function bubbleSort(array) {
+    //avg runtime O(n^2), already sorted array is O(n) which is good
+    const length = array.length;
+    let swapped = false;
+  
+    for (let i = 0; i < length; i++) {
+      //start iterative loop that runs n times
+  
+      swapped = false; //set boolean to check if the array is already sorted
+  
+      for (let j = 0; j < length; j++) {
+        // second iteration to compare each element of the array and swap the smaller one to the left, or do nothing if the smaller is already left
+  
+        if (array[j] > array[j + 1]) {
+          let temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          swapped = true; //if a swap is made continue the loop
+        }
+      }
+  
+      if (!swapped) {
+        //once no elements are swapper or the main loop runs n number of times break
+        return;
+      }
+    }
+  }
+
+
+export const selectionSort = (array) => {
+  //avg runtime O(n^2), best case also O(n^2), slightly better than bubble and gnome
+
+  let length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    let min = i; //set min to element in position i for current iteration of the main loop
+    for (let j = i + 1; j < length; j++) {
+      //start second iteration comparing each element to the current minimum starting with i (current iteration of the main loop)
+      if (array[j] < array[min]) {
+        //if an element after element i is less than the currently recorded min, set that position of the array as current min
+        min = j;
+      }
+    }
+    if (min !== i) {
+      //if first element is already the minimum don't swap
+      let temp = array[i]; //else swap position of current min element with first element of current iteration
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
+};
+
+export const insertionSort = (array) => {
+  //avg runtime O(n^2), fast for checking if an array is sorted, or if sample size is small, best quadratic sorting alg in most cases
+
+  let length = array.length;
+
+  for (let i = 1; i < length; i++) {
+    //main loop for each element in array, starts at 1 so first element is already in the "sorted" array
+
+    let current = array[i]; //current element of main loop
+
+    let j = i - 1; //compare to previous element of our array
+
+    while (i > -1 && current < array[j]) {
+      //if current element is both in the array and less than previous element
+      array[j + 1] = array[j]; //push larger element right
+      j--; //move to the previous element in the array
+    }
+    array[j + 1] = current; //place the current element one step lower in the array
+  }
+  return array;
+};
+
+export const gnomeSort = (array) => {
+  //avg runtime O(n^2), performs better on partially sorted arrays, generally pretty bad
+  let length = array.length;
+
+  let index = 0;
+
+  while (index < length) {
+    //scan through the entire array
+    if (index === 0) {
+      //if you are at the end move up
+      index++;
+    }
+    if (array[index] >= array[index - 1]) {
+      //if you are not at the end and the current element is greater than the previous one move up
+      index++;
+    } else {
+      //if you are not at the end and the current element is less than the previous element swap them and move down to compare again
+      let temp = 0;
+      temp = array[index];
+      array[index] = array[index - 1];
+      array[index - 1] = temp;
+      index--;
+    }
+  }
+  return array;
+};
+
+export let mergeSortAnimations = (array) => {
+  const animations = [];
+  if (array.length <= 1) return array;
+  const auxiliaryArray = array.slice();
+  mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+  return animations;
+};
+
+function mergeSortHelper(
+  mainArray,
+  startIdx,
+  endIdx,
+  auxiliaryArray,
+  animations
+) {
+  if (startIdx === endIdx) return;
+  const middleIdx = Math.floor((startIdx + endIdx) / 2);
+  mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+  doMergeSort(
+    mainArray,
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArray,
+    animations
+  );
+}
+
+function doMergeSort(
+  mainArray,
+  startIndx,
+  middleIndx,
+  endIndx,
+  auxiliaryArray,
+  animations
+) {
+  let k = startIndx;
+  let i = startIndx;
+  let j = middleIndx + 1;
+  while (i <= middleIndx && j <= endIndx) {
+    // These are the values that we're comparing; we push them once
+    // to change their color.
+    animations.push([i, j]);
+    // These are the values that we're comparing; we push them a second
+    // time to revert their color.
+    animations.push([i, j]);
+    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+      // We overwrite the value at index k in the original array with the
+      // value at index i in the auxiliary array.
+      animations.push([k, auxiliaryArray[i]]);
+      mainArray[k++] = auxiliaryArray[i++];
+    } else {
+      // We overwrite the value at index k in the original array with the
+      // value at index j in the auxiliary array.
+      animations.push([k, auxiliaryArray[j]]);
+      mainArray[k++] = auxiliaryArray[j++];
+    }
+  }
+  while (i <= middleIndx) {
+    // These are the values that we're comparing; we push them once
+    // to change their color.
+    animations.push([i, i]);
+    // These are the values that we're comparing; we push them a second
+    // time to revert their color.
+    animations.push([i, i]);
+    // We overwrite the value at index k in the original array with the
+    // value at index i in the auxiliary array.
+    animations.push([k, auxiliaryArray[i]]);
+    mainArray[k++] = auxiliaryArray[i++];
+  }
+  while (j <= endIndx) {
+    // These are the values that we're comparing; we push them once
+    // to change their color.
+    animations.push([j, j]);
+    // These are the values that we're comparing; we push them a second
+    // time to revert their color.
+    animations.push([j, j]);
+    // We overwrite the value at index k in the original array with the
+    // value at index j in the auxiliary array.
+    animations.push([k, auxiliaryArray[j]]);
+    mainArray[k++] = auxiliaryArray[j++];
+  }
+}
---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,211 @@
+import React from "react";
+import { mergeSortAnimations } from "../SortingAlgorithms/MergeSort.js";
+import { getBubbleSort } from "../SortingAlgorithms/BubbleSort.js";
+import * as shit from "../SortingAlgorithms/MergeSort.js";
+import "./SortingVisualizer.css";
+
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    let i = 0;
+    while (i <= 420) {
+      array.push(randomIntFromInterval(6, 800));
+      i++;
+    }
+    this.setState({ array });
+  }
+
+  linearArray() {
+    const array = [];
+    let i = 0;
+    let x = 5;
+    while (i <= 420) {
+      array.push(x);
+      i++;
+      x += 2;
+    }
+    this.shuffle(array);
+
+    this.setState({ array });
+  }
+
+  shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  render() {
+    const { array } = this.state;
+
+    return (
+      <div className="container">
+        <div className="array_container">
+          {array.map((value, idx) => (
+            <div
+              className="array_bar"
+              key={idx}
+              style={{ height: `${value}px` }}
+            ></div>
+          ))}
+        </div>
+        <button onClick={() => this.resetArray()}>
+          Generate New Random Array
+        </button>
+        <button onClick={() => this.linearArray()}>
+          Generate New Linear Array
+        </button>
+        <button onClick={() => this.bubbleSort()}> Bubble sort </button>
+        <button onClick={() => this.insertionSort()}> Insertion Sort </button>
+        <button onClick={() => this.gnomeSort()}> Gnome Sort </button>
+        <button onClick={() => this.selectionSort()}> Selection Sort </button>
+        <button onClick={() => this.mergeSort()}> Merge Sort </button>
+        <button onClick={() => this.quickSort()}> Quick Sort </button>
+        <button onClick={() => this.timSort()}> Tim Sort </button>
+        <button onClick={() => this.testSortingAlgorithms()}>
+          Test Sorting Algorithms
+        </button>
+      </div>
+    );
+  }
+
+  insertionSort() {
+    const javaScriptSortedArray = this.state.array
+      .slice()
+      .sort((a, b) => a - b);
+    const sortedArray = shit.insertionSort(this.state.array);
+
+    console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+  }
+
+  gnomeSort() {
+    const javaScriptSortedArray = this.state.array
+      .slice()
+      .sort((a, b) => a - b);
+    const sortedArray = shit.gnomeSort(this.state.array);
+
+    console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+  }
+
+  selectionSort() {
+    const javaScriptSortedArray = this.state.array
+      .slice()
+      .sort((a, b) => a - b);
+    const sortedArray = shit.selectionSort(this.state.array);
+
+    console.log(arraysAreEqual(javaScriptSortedArray, sortedArray));
+  }
+
+  bubbleSort() {
+    const animations = getBubbleSort(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array_bar");
+      const isColorChange = i % 4 === 0 || i % 4 === 1;
+      if (isColorChange) {
+        const color = i % 4 === 0 ? "red" : "royalblue";
+        let [bar1Indx, bar2Indx] = animations[i];
+        if (bar2Indx>420) {
+            bar2Indx = 420;
+        }
+        const bar1Style = arrayBars[bar1Indx].style;
+        const bar2Style = arrayBars[bar2Indx].style;
+        setTimeout(() => {
+          bar1Style.backgroundColor = color;
+          bar2Style.backgroundColor = color;
+        }, 1);
+      } else {
+        const [bar1Indx, newHeight] = animations[i];
+        if (bar1Indx === -1) {
+          continue;
+        }
+        setTimeout(() => {
+          const barStyle = arrayBars[bar1Indx].style;
+          barStyle.height = `${newHeight}px`;
+        }, 1);
+      }
+    }
+  }
+
+  mergeSort() {
+    const animations = mergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array_bar");
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIndx, barTwoIndx] = animations[i];
+        const barOneStyle = arrayBars[barOneIndx].style;
+        const barTwoStyle = arrayBars[barTwoIndx].style;
+        const color = i % 3 === 0 ? "red" : "royalblue";
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i*3);
+      } else {
+        setTimeout(() => {
+          const [barOneIndx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIndx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i*3);
+      }
+    }
+  }
+
+  quickSort() {}
+
+  timSort() {}
+
+  testSortingAlgorithms() {
+    for (let i = 0; i < 100; i++) {
+      const array = [];
+      const legnth = randomIntFromInterval(1, 1000);
+      for (let i = 0; i < legnth; i++) {
+        array.push(randomIntFromInterval(-1000, 1000));
+      }
+      const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+      const gnomeSortedArray = shit.gnomeSort(array.slice());
+      const insertionSortedArray = shit.insertionSort(array.slice());
+      const selectionSortedArray = shit.selectionSort(array.slice());
+      console.log(arraysAreEqual(javaScriptSortedArray, gnomeSortedArray));
+      console.log(arraysAreEqual(javaScriptSortedArray, insertionSortedArray));
+      console.log(arraysAreEqual(javaScriptSortedArray, selectionSortedArray));
+    }
+  }
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arraysAreEqual(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+}
